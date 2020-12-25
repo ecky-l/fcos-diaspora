e45497b32f25d7a66f82ab6617a968d07f1939aa
ecky-l <ecky-l@users.noreply.github.com>
2020-12-25 12:03:01 +0100

    switched from pod service to container services with static IPs

--- modules/fcos-diaspora/main.tf
+++ modules/fcos-diaspora/main.tf
@@ -21,10 +21,17 @@ locals {
   initramfs = local.cached_initrd
   initrootfs = local.cached_rootfs
   args   = local.boot_args
+
+  postgresql_ip = "10.88.0.5"
+  redis_ip = "10.88.0.6"
+  diaspora_ip = "10.88.0.7"
+  nginx_ip = "10.88.0.8"
+  smtpd_ip = "10.88.0.9"
 }
 
 resource "random_password" "postgresql-password" {
   length = 24
+  special = false
 }
 
 data "ct_config" "diaspora_ignition" {
@@ -39,17 +46,26 @@ data "ct_config" "diaspora_ignition" {
     templatefile("${path.module}/templates/diaspora-config.yaml", {
       postgres_password = random_password.postgresql-password.result
       diaspora_server_name = var.diaspora_server_name
+      postgresql_ip = local.postgresql_ip
+      redis_ip = local.redis_ip
+      smtpd_ip = local.smtpd_ip
     }),
     templatefile("${path.module}/templates/nginx-config.yaml", {
       diaspora_server_name = var.diaspora_server_name
+      diaspora_ip = local.diaspora_ip
     }),
     templatefile("${path.module}/templates/letsencrypt.yaml", {
       diaspora_server_name = var.diaspora_server_name
       letsencrypt_email = var.letsencrypt_email
     }),
-    templatefile("${path.module}/templates/diaspora-pod.yaml", {
+    templatefile("${path.module}/templates/diaspora-services.yaml", {
       postgres_password = random_password.postgresql-password.result
       diaspora_server_name = var.diaspora_server_name
+      postgresql_ip = local.postgresql_ip
+      redis_ip = local.redis_ip
+      diaspora_ip = local.diaspora_ip
+      nginx_ip = local.nginx_ip
+      smtpd_ip = local.smtpd_ip
     })
   ]
   )
